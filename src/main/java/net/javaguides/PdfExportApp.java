160416e42842e9c9eadddde1332fe75239690a4d
Abram <tacksontiger@gmail.com>
2025-11-17 23:06:08 +0300

    commit #4
    I implemented PDF export functionality using iText with formatted table output and added itextpdf dependency.

--- src/main/java/net/javaguides/PdfExportApp.java
+++ src/main/java/net/javaguides/PdfExportApp.java
@@ -1,10 +1,18 @@
 package net.javaguides;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.pdf.*;
 import javax.swing.*;
 import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class PdfExportApp extends JFrame {
     JTable table;
@@ -30,6 +38,7 @@ public class PdfExportApp extends JFrame {
         add(sp, BorderLayout.CENTER);
 
         loadBtn.addActionListener(this::loadDataFromDB);
+        exportBtn.addActionListener(this::exportPDF);
         setVisible(true);
     }
 
@@ -76,5 +85,67 @@ public class PdfExportApp extends JFrame {
 
         return new javax.swing.table.DefaultTableModel(data, columnNames);
     }
+    private void exportPDF(ActionEvent e) {
+        try {
+            // Check empty table
+            if (table.getRowCount() == 0) {
+                JOptionPane.showMessageDialog(this, "Table is empty! Nothing to export.");
+                return;
+            }
 
+            JFileChooser chooser = new JFileChooser();
+            chooser.setDialogTitle("Save PDF");
+            chooser.setSelectedFile(new File("report.pdf"));
+
+            if (chooser.showSaveDialog(this) != JFileChooser.APPROVE_OPTION)
+                return;
+
+            File file = chooser.getSelectedFile();
+            Document document = new Document(PageSize.A4);
+            PdfWriter.getInstance(document, new FileOutputStream(file));
+
+            document.open();
+
+            // Title
+            Font titleFont = new Font(Font.FontFamily.HELVETICA, 18, Font.BOLD);
+            Paragraph title = new Paragraph("Employee Report", titleFont);
+            title.setAlignment(Element.ALIGN_CENTER);
+            document.add(title);
+
+            // Timestamp
+            String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+            Paragraph timestamp = new Paragraph("Generated on: " + date);
+            timestamp.setAlignment(Element.ALIGN_CENTER);
+            timestamp.setSpacingAfter(20);
+            document.add(timestamp);
+
+            // Table
+            PdfPTable pdfTable = new PdfPTable(table.getColumnCount());
+            pdfTable.setWidthPercentage(100);
+
+            // Column Headers
+            TableModel model = table.getModel();
+            Font headerFont = new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD);
+
+            for (int i = 0; i < model.getColumnCount(); i++) {
+                PdfPCell header = new PdfPCell(new Phrase(model.getColumnName(i), headerFont));
+                header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+                header.setHorizontalAlignment(Element.ALIGN_CENTER);
+                pdfTable.addCell(header);
+            }
+
+            // Table Data
+            for (int r = 0; r < model.getRowCount(); r++) {
+                for (int c = 0; c < model.getColumnCount(); c++) {
+                    pdfTable.addCell(new Phrase(model.getValueAt(r, c).toString()));
+                }
+            }
+
+            document.add(pdfTable);
+            document.close();
+
+            JOptionPane.showMessageDialog(this, "PDF Exported Successfully!");
+
+        }
+    }
 }
