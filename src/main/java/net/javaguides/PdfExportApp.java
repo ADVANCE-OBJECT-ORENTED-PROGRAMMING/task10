ac940ce8af140e4c500bb35dc2808ba1d6ed0540
Abram <tacksontiger@gmail.com>
2025-11-17 22:39:37 +0300

    commit #2
     I implemented MySQL connection and loaded employees data into JTable

--- src/main/java/net/javaguides/PdfExportApp.java
+++ src/main/java/net/javaguides/PdfExportApp.java
@@ -1,7 +1,10 @@
 package net.javaguides;
 
 import javax.swing.*;
+import javax.swing.table.TableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.sql.*;
 
 public class PdfExportApp extends JFrame {
     JTable table;
@@ -26,7 +29,30 @@ public class PdfExportApp extends JFrame {
         add(topPanel, BorderLayout.NORTH);
         add(sp, BorderLayout.CENTER);
 
+        loadBtn.addActionListener(this::loadDataFromDB);
         setVisible(true);
+    }
+
+    private void loadDataFromDB(ActionEvent e) {
+        try {
+            String url = "jdbc:mysql://localhost:3306/task10";
+            String user = "root";
+            String pass = "witty";
+
+            Connection conn = DriverManager.getConnection(url, user, pass);
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT id, name, email FROM employees");
+
+            conn.close();
+            JOptionPane.showMessageDialog(this, "Data Loaded Successfully!");
+
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(this,
+                    "Error loading data: " + ex.getMessage(),
+                    "Database Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+
     }
 
 }
