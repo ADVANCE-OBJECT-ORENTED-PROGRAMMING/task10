da8fc50582da40af2d5ccde20a5c6967f7bce1ef
Abram <tacksontiger@gmail.com>
2025-11-17 22:56:07 +0300

    commit #2
    Added dynamic ResultSet-to-TableModel converter for JTable

--- src/main/java/net/javaguides/PdfExportApp.java
+++ src/main/java/net/javaguides/PdfExportApp.java
@@ -43,6 +43,7 @@ public class PdfExportApp extends JFrame {
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery("SELECT id, name, email FROM employees");
 
+            table.setModel(buildTableModel(rs));
             conn.close();
             JOptionPane.showMessageDialog(this, "Data Loaded Successfully!");
 
@@ -53,6 +54,27 @@ public class PdfExportApp extends JFrame {
                     JOptionPane.ERROR_MESSAGE);
         }
 
+    }
+    public static javax.swing.table.TableModel buildTableModel(ResultSet rs) throws Exception {
+
+        ResultSetMetaData meta = rs.getMetaData();
+        int columnCount = meta.getColumnCount();
+
+        java.util.Vector<String> columnNames = new java.util.Vector<>();
+        for (int i = 1; i <= columnCount; i++) {
+            columnNames.add(meta.getColumnName(i));
+        }
+
+        java.util.Vector<java.util.Vector<Object>> data = new java.util.Vector<>();
+        while (rs.next()) {
+            java.util.Vector<Object> row = new java.util.Vector<>();
+            for (int i = 1; i <= columnCount; i++) {
+                row.add(rs.getObject(i));
+            }
+            data.add(row);
+        }
+
+        return new javax.swing.table.DefaultTableModel(data, columnNames);
     }
 
 }
